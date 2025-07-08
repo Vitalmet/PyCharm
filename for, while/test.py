4cf0b670dbfba7ab36f4543071924919280e8dc2
Vitalmet <vuyko87@bk.ru>
2025-07-08 12:33:32 +0300

    Init commit

--- for, while/test.py
+++ for, while/test.py
@@ -1,13 +1,1 @@
-'''total = 0
-for count in range(1, 6):
-    total = total + count
-    print(total)'''
-
-flower1 = 4
-flower2 = 3
-
-def love_func(flower1, flower2):
-    if flower1 / 2 == 0 and flower2 / 2 != 0:
-        return True
-    else:
-        return False
+print("Hello, World!")
