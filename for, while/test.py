1987827418e2dff935364590616f4dd643e9bca3
Vitalmet <vuyko87@bk.ru>
2025-07-15 12:57:24 +0300

    rename "Algoriphms" and make some code in "test.py"

---
+++ for, while/test.py
@@ -0,0 +1,9 @@
+''' product = 0  # Инициализация переменной
+
+while product < 100:
+    number = int(input("Введите число: "))
+    product = number * 10
+    print(f"Результат: {product}")
+
+print("Цикл завершён (product >= 100)") '''
+
