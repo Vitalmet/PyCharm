d9bc0d23960a46cb2ef3f544134bcd7630e21c32
Vitalmet <vuyko87@bk.ru>
2025-08-19 20:04:37 +0300

    made an assert design "test_capitalize"

--- my_project/test/test_capitalize.py
+++ my_project/test/test_capitalize.py
@@ -4,10 +4,5 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../s
 
 from capitalize import capitalize
 
-if capitalize("hello") != "Hello":
-    raise Exception("Функция работает неверно!")
-
-if capitalize("") != "":
-    raise Exception("Функция работает неверно!")
-
-print("Все тесты пройдены!")
+assert capitalize("hello") == "Hello"
+assert capitalize("") == ""
