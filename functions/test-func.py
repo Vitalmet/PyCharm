18eb5438f0e8131fa186ece907faadd79359716e
Vitalmet <vuyko87@bk.ru>
2025-09-07 10:56:29 +0300

    made some practice

--- functions/test-func.py
+++ functions/test-func.py
@@ -51,3 +51,26 @@ for i in range(N):
 
 print(arr)
 """
+"""def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+answer = factorial(int(input()))
+print(answer)
+"""
+
+def flatten(nested_list):
+    flat_list = []
+
+    for item in nested_list:
+        if isinstance(item, list):
+            flat_list.extend(flatten(item))
+        else:
+            flat_list.append(item)
+
+    return flat_list
+
+nested = [1, [2, [3, 4], 5], [6, 7], [1, 2, 3, 4, [5], [], 10 ]]
+print(flatten(nested))
