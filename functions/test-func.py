5c320343a47d4206f984752885a0e40e9513853b
Vitalmet <vuyko87@bk.ru>
2025-09-26 18:27:08 +0300

    made some practice

--- functions/test-func.py
+++ functions/test-func.py
@@ -116,6 +116,27 @@ def aggregate(items):
 
 print(aggregate(items))
 """
-raising_to_a_power = 3 ** 5
-divisor = -8 / -4
-print(f"{raising_to_a_power}{divisor}\n")
+class Person:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    @property
+    def full_name(self):
+        return self.name + " " + self.surname
+
+    @full_name.setter
+    def full_name(self, new):
+        self.name, self.surname = new.split(" ")
+
+    @full_name.deleter
+    def full_name(self):
+        print("Удаляем имя и фамилию.")
+        self.name = None
+        self.surname = None
+
+tom = Person("Tom", "Smith")
+del tom.full_name
+print(tom.surname)
+
+
