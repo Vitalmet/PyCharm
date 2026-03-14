38fa72b97728336042604f021977885d2075c9b7
Vitalmet <vuyko87@bk.ru>
2026-03-14 16:51:13 +0300

    I'm learning programming

--- Python_SQL/test_sql_python.py
+++ Python_SQL/test_sql_python.py
@@ -1,14 +1,19 @@
-import psycopg2
+# Урок 2
+def add_movies(conn):
+    # Исправлено: второй фильм - The Green Mile
+    ins_movi = "INSERT INTO movies (title, duration, release_year) VALUES ('Godfather', 175, 1972)"
+    ins_movi2 = "INSERT INTO movies (title, duration, release_year) VALUES ('The Green Mile', 189, 1999)"
 
-try:
-    # пытаемся подключиться к базе данных
-    conn = psycopg2.connect(
-        dbname="hexlet_db",
-        user="vital",
-        password="Kurt1994!",
-        host="localhost",
-        port="5432"
-    )
-except:
-    # в случае сбоя подключения будет выведено сообщение  в STDOUT
-    print("Can`t establish connection to database")
+    cursor = conn.cursor()
+    cursor.execute(ins_movi)
+    cursor.execute(ins_movi2)
+    conn.commit()
+    cursor.close()
+
+
+def get_all_movies(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, title, release_year, duration FROM movies ORDER BY id;")
+    movies = cursor.fetchall()
+    cursor.close()
+    return movies
