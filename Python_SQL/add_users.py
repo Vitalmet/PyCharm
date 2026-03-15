cc2bf2d1855732c6a45e53073903ddca25fdc5f4
Vitalmet <vuyko87@bk.ru>
2026-03-15 10:26:24 +0300

    I'm learning programming

---
+++ Python_SQL/add_users.py
@@ -0,0 +1,33 @@
+import psycopg2
+from psycopg2.extras import execute_batch, execute_values
+
+# Подключаемся к базе
+conn = psycopg2.connect(
+    dbname="hexlet_db",
+    user="vital",
+    password="Kurt1994!",
+    host="localhost"
+)
+
+# Создаем курсор
+curs = conn.cursor()
+
+# Вариант с execute_batch
+users = (
+    ("Bob", "232435"),
+    ("Alice", "0987"),
+    ("John", "234646757"),
+)
+execute_batch(curs, "INSERT INTO users (username, phone) VALUES (%s, %s)", users)
+
+# Сохраняем изменения
+conn.commit()
+
+# Проверяем, что добавилось
+curs.execute("SELECT * FROM users;")
+for row in curs.fetchall():
+    print(row)
+
+# Закрываем соединение
+curs.close()
+conn.close()
--- Python_SQL/test_sql_python.py
+++ Python_SQL/test_sql_python.py
@@ -33,3 +33,124 @@ def get_all_cars(conn):
     cursor.close()
     return all_cars
 # END
+
+# или можно решить так при помощи контекстнного менеджера
+
+import psycopg2
+
+conn = psycopg2.connect('postgresql://tirion:secret@localhost:5432/tirion')
+
+
+# BEGIN
+def get_all_cars(conn):
+    with conn.cursor() as c:
+        sql = "SELECT * FROM cars ORDER BY brand ASC;"
+        c.execute(sql)
+        result = c.fetchall()
+    return result
+# END
+
+# урок 4
+'''name = "John"
+age = 19
+
+with conn.cursor() as curs:
+    # для позиционных аргументов всегда передается последовательность, даже если параметр один
+    # здесь передается кортеж (name,)
+    curs.execute("SELECT id, name FROM users WHERE name=%s;", (name,))
+    curs.fetchall()
+
+with conn.cursor() as curs:
+    # также можно использовать именованные аргументы
+    curs.execute(
+        "INSERT INTO users (name, age) VALUES (%(name)s, %(age)s);",
+        {"age": age, "name": name},
+    )
+    conn.commit()
+
+conn.close()
+
+Ускорение запросов
+
+from psycopg2.extras import execute_batch, execute_values
+
+users = (
+    ("Bob", "bob@mail.com"),
+    ("Alice", "alice@mail.com"),
+    ("John", "john@mail.com"),
+)
+execute_batch(curs, "INSERT INTO users (name, email) VALUES (%s, %s)", users)
+
+# в случае execute_values запрос будет выглядеть так
+users = [
+    ("Bob", "bob@mail.com"),
+    ("Alice", "alice@mail.com"),
+    ("John", "john@mail.com"),
+]
+execute_values(curs, "INSERT INTO users (name, email) VALUES %s", users)
+
+Возврат идентификатора
+
+user = User()
+# Сохраняем пользователя в базу данных
+# После этого становится доступен id
+id = user.get_id()
+# Его можно использовать для формирования ссылок или вставки связанных записей
+
+# RETURNING возвращает указанное поле
+ with conn.cursor() as cur:
+        cur.execute(
+            "INSERT INTO users (name, email) VALUES (%s, %s) RETURNING id;",
+            (user.name, user.email)
+        )
+        user.id = cur.fetchone()[0]
+conn.close()'''
+
+import psycopg2
+from psycopg2.extras import execute_values
+
+conn = psycopg2.connect('postgresql://tirion:secret@localhost:5432/tirion')
+
+
+# BEGIN (write your solution here)
+def batch_insert(conn, products):
+    with conn.cursor() as curs:
+        # Преобразуем словари в кортежи
+        products_data = [(p['name'], p['price'], p['quantity']) for p in products]
+        execute_values(curs, "INSERT INTO products (name, price, quantity) VALUES %s", products_data)
+    conn.commit()
+
+
+def get_all_products(conn):
+    with conn.cursor() as curs:
+        curs.execute("SELECT * FROM products ORDER BY price DESC;")
+        return curs.fetchall()
+# END
+
+import psycopg2
+from psycopg2.extras import execute_values
+
+conn = psycopg2.connect('postgresql://tirion:secret@localhost:5432/tirion')
+
+
+# BEGIN
+def batch_insert(conn, products):
+    with conn.cursor() as cur:
+        values = [(p['name'], p['price'], p['quantity']) for p in products]
+
+        insert_query = "INSERT INTO products (name, price, quantity) VALUES %s"
+
+        execute_values(cur, insert_query, values)
+    conn.commit()
+
+
+def get_all_products(conn):
+    with conn.cursor() as cur:
+        sql = "SELECT * FROM products ORDER BY price DESC;"
+        cur.execute(sql)
+        result = cur.fetchall()
+    conn.commit()
+    return result
+# END
+
+
