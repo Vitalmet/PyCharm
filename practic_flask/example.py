8b34e636e675ab940322f6264c89a2969b539564
Vitalmet <vuyko87@bk.ru>
2026-03-23 20:07:37 +0300

    I'm learning programming

--- practic_flask/example.py
+++
@@ -1,6 +0,0 @@
-def app(environ, start_response):
-    data = b"What the heck?"
-    start_response(
-        "200 OK", [("Content-Type", "text/plain"), ("Content-Length", str(len(data)))]
-    )
-    return iter([data])
