43fb9a7b9ec174181a641f8c26279de8bbb6501e
Vitalmet <vuyko87@bk.ru>
2026-03-27 19:57:04 +0300

    I'm learning programming

--- hexlet-flask-example/example.py
+++ hexlet-flask-example/example.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template
 
 # Это callable WSGI-приложение
 app = Flask(__name__)
@@ -20,3 +20,11 @@ def users():
 def courses_show(id):
     return f"Course id: {id}"
 
+
+@app.route("/users/<int:user_id>")
+def show_user(user_id):
+    user = {
+        "id": user_id,
+        "name": f"user-{user_id}"
+    }
+    return render_template("users/show.html", user=user)
