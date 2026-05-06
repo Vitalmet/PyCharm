d4622463a47fb662395b563e486121e48d961d0b
Vitalmet <vuyko87@bk.ru>
2026-05-06 19:44:13 +0300

    Сделал views.py, шаблоны и настройки в settings.py

--- hexlet-django-blog/hexlet_django_blog/urls.py
+++ hexlet-django-blog/hexlet_django_blog/urls.py
@@ -1,22 +1,30 @@
 """
-URL configuration for hexlet_django_blog project.
+Конфигурация URL для проекта hexlet_django_blog.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+Список `urlpatterns` направляет URL-адреса к представлениям. Для получения дополнительной информации см.:
+
+https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Примеры:
+Функциональные представления
+1. Добавьте импорт: from my_app import views
+2. Добавьте URL в urlpatterns: path('', views.home, name='home')
+Представления на основе классов
+
+1. Добавьте импорт: from other_app.views import Home
+
+2. Добавьте URL в urlpatterns: path('', Home.as_view(), name='home')
+Включение еще одной конфигурации URL
+
+1. Импортируйте функцию include(): from django.urls import include, path
+
+2. Добавьте URL в urlpatterns: path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
+from hexlet_django_blog import views
 
 urlpatterns = [
+    path("", views.index),
+    path("about/", views.about),
     path('admin/', admin.site.urls),
 ]
