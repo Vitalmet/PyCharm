4e1565ad5b98dd46bc5be6a5942431a99323c95d
Vitalmet <vuyko87@bk.ru>
2026-05-14 20:09:17 +0300

    работа в проекте hexlet_django_blog

--- hexlet-django-blog/hexlet_django_blog/urls.py
+++ hexlet-django-blog/hexlet_django_blog/urls.py
@@ -20,11 +20,12 @@ https://docs.djangoproject.com/en/6.0/topics/http/urls/
 2. Добавьте URL в urlpatterns: path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from hexlet_django_blog import views
 
 urlpatterns = [
     path("", views.index),
     path("about/", views.about),
+    path('articles/', include('hexlet_django_blog.article.urls')),
     path('admin/', admin.site.urls),
 ]
