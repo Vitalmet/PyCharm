aace17c95cab9ac48d274f654b865c95b1d538b5
Vitalmet <vuyko87@bk.ru>
2026-05-16 08:18:22 +0300

    работа в проекте hexlet_django_blog

--- hexlet-django-blog/hexlet_django_blog/article/views.py
+++ hexlet-django-blog/hexlet_django_blog/article/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("article")
+    context = {
+        'app_name': 'hexlet-django-blog',
+        'description': 'Это приложение для статей блога'
+    }
+    return render(request, 'articles/index.html', context)
