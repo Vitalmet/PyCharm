75e7da65703b3d96fce41deacf51971c3f28fa85
Vitalmet <vuyko87@bk.ru>
2026-05-28 19:43:22 +0300

    работа в проекте hexlet_django_blog

--- hexlet-django-blog/hexlet_django_blog/article/urls.py
+++ hexlet-django-blog/hexlet_django_blog/article/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from hexlet_django_blog.article.views import IndexView
+from hexlet_django_blog.article.views import IndexView, ArticleView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='articles'),
-    path('<int:id>/', IndexView.as_view(), name='article'),
+    path("<int:id>/", ArticleView.as_view(), name='article'),
 ]
--- hexlet-django-blog/hexlet_django_blog/article/views.py
+++ hexlet-django-blog/hexlet_django_blog/article/views.py
@@ -1,5 +1,5 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from hexlet_django_blog.article.models import Article
 
@@ -11,5 +11,16 @@ class IndexView(View):
             "articles/index.html",
             context={
                 "articles": articles
+            },
+        )
+
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs["id"])
+        return render(
+            request,
+            "articles/show.html",
+            context={
+                "article": article,
             },
         )
