71f0a368f2143ca5ffed54897e191401980201ee
Vitalmet <vuyko87@bk.ru>
2026-06-02 19:17:06 +0300

    работа в проекте hexlet_django_blog

--- hexlet-django-blog/hexlet_django_blog/article/urls.py
+++ hexlet-django-blog/hexlet_django_blog/article/urls.py
@@ -4,6 +4,7 @@ from hexlet_django_blog.article.views import (
     ArticleView,
     ArticleFormCreateView,
     ArticleFormEditView,
+    ArticleFormDeleteView,
 )
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path("<int:id>/edit/", ArticleFormEditView.as_view(), name="articles_update"), # редактирование
     path("<int:id>/", ArticleView.as_view(), name='article'), # просмотр
     path("create/", ArticleFormCreateView.as_view(), name="articles_create"), # создание
+    path("<int:id>/delete/", ArticleFormDeleteView.as_view(), name="articles_delete"), # удаление
 ]
